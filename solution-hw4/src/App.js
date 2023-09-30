a424c318c627bac21a971d7cb9e73c8aad2540db
sunnyvicks <143432796+sunnyvicks@users.noreply.github.com>
2023-09-30 14:39:49 -0400

    hw4 update structure to match assignment req

--- solution-hw4/src/App.js
+++ solution-hw4/src/App.js
@@ -1,75 +1,7 @@
-import "./App.css";
-import { PRODUCTS } from "utils";
-import ProdItem from "components/ProdItem";
-import Navbar from "components/Navbar";
-import { useRef, useState } from "react";
+import Home from "views/home";
 
 function App() {
-  const [cart, setCart] = useState([]);
-  const [popup, setPopup] = useState(null);
-  const timer = useRef(null);
-
-  // checker
-  const checkCartItem = ({ name, packSize, glazing }) => {
-    // didn't select packSize
-    if (!packSize) {
-      return "invalid_entry";
-    }
-    const cartItemIdx = cart.findIndex((item) => item.name === name);
-    const cartItem = cart[cartItemIdx];
-    // duplicate entry
-    if (
-      cartItem &&
-      cartItem.name === name &&
-      cartItem.packSize === packSize &&
-      cartItem.glazing === glazing
-    ) {
-      return "invalid_entry";
-    }
-    // new entry or existing cart item
-    return cartItem ? cartItemIdx : "new_entry";
-  };
-
-  const handleAddCart = (product) => {
-    // // check if cart should update
-    const cartCheck = checkCartItem(product);
-    if (cartCheck === "invalid_entry") {
-      return;
-    }
-    // update or add new entry to cart
-    if (cartCheck === "new_entry") {
-      setCart((prev) => [...prev, product]);
-    } else {
-      setCart((prev) => {
-        prev[cartCheck] = product;
-        return prev;
-      });
-    }
-    setPopup(product);
-    clearTimeout(timer.current);
-    timer.current = setTimeout(() => {
-      setPopup(null);
-    }, 3000);
-  };
-
-  return (
-    <div className="App">
-      <Navbar popup={popup} cart={cart} />
-      {/* List of products */}
-      <main className="content">
-        <ul className="products">
-          {PRODUCTS.map((prod, idx) => (
-            <ProdItem
-              key={idx}
-              product={prod}
-              id={idx}
-              handleAddCart={handleAddCart}
-            />
-          ))}
-        </ul>
-      </main>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
